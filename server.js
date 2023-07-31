c6f413628a90a76166126d529710f8eae6d38199
Christopher Donner <cdonner@opentext.com>
2023-07-31 15:29:58 -0400

    base functionality

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 
-var app = express();
+
+app = express();
 
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
@@ -15,16 +16,16 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var mysql = require("mysql");
+var mysql = require("mysql"),
 
-var connection = mysql.createConnection({
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "P@ssw0rd",
   database: "coyote_db"
 });
-
+  
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -34,7 +35,7 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
-// // Use Handlebars to render the main index.html page with the burgers in it.
+// // Use Handlebars to render the main index.html page with the coyotes in it.
 app.get("/", function(req, res) {
   connection.query("SELECT * FROM coyotes where active=false", function(err, data) {
     if (err) {
@@ -43,28 +44,28 @@ app.get("/", function(req, res) {
     connection.query("select * from coyotes where active=true", function(err, resp){
     // console.log(data)
     // console.log(req.body,res.body)
-    res.render("index", { burgers: data, oldBurgers: resp });
+    res.render("index", { coyotes: data, oldcoyotes: resp });
     })
   });
 });
 
-// // Create a new burger
-// app.post("/Burgers", function(req, res) {
-//     console.log(req.body)
-//   connection.query("INSERT INTO burgers (burgerName, devoured) VALUES (?, false)", [req.body.Burger], function(err, result) {
-//     if (err) {
-//       return res.status(503).end();
-//     }
+// // Create a new coyote
+app.post("/Coyotes", function(req, res) {
+    console.log(req.body)
+  connection.query("INSERT INTO coyotes (coyoteName, active) VALUES (?, false)", [req.body.coyote], function(err, result) {
+    if (err) {
+      return res.status(503).end();
+    }
 
-//     // Send back the ID of the new burger
-//     res.json({ id: result.insertId });
-//     console.log({ id: result.insertId });
-//   });
-// });
+    // Send back the ID of the new coyote
+    res.json({ id: result.insertId });
+    console.log({ id: result.insertId });
+  });
+});
 
-// // Retrieve all burgers
-// app.get("/Burgers", function(req, res) {
-//   connection.query("SELECT * FROM burgers;", function(err, data) {
+// Retrieve all coyotes
+// app.get("/Coyotes", function(req, res) {
+//   connection.query("SELECT * FROM coyotes;", function(err, data) {
 //     if (err) {
 //       return res.status(500).end();
 //     }
@@ -73,10 +74,10 @@ app.get("/", function(req, res) {
 //   });
 // });
 
-// // Update a burger
-// app.put("/Burgers/:id", function(req, res) {
+// // Update a coyote
+// app.put("/Coyotes/:id", function(req, res) {
 //   console.log(req.params.id)
-//   connection.query("UPDATE burgers SET devoured = true WHERE id = ?", [req.params.id], function(err, result) {
+//   connection.query("UPDATE coyotes SET active = true WHERE id = ?", [req.params.id], function(err, result) {
 //     if (err) {
 //       // If an error occurred, send a generic server failure
 //       return res.status(500).end();
@@ -90,9 +91,9 @@ app.get("/", function(req, res) {
 //   });
 // });
 
-// // Delete a burger
-// app.delete("/burgers/:id", function(req, res) {
-//   connection.query("DELETE FROM burgers WHERE id = ?", [req.params.id], function(err, result) {
+// // Delete a coyote
+// app.delete("/coyotes/:id", function(req, res) {
+//   connection.query("DELETE FROM coyotes WHERE id = ?", [req.params.id], function(err, result) {
 //     if (err) {
 //       // If an error occurred, send a generic server failure
 //       return res.status(500).end();
@@ -111,3 +112,28 @@ app.listen(PORT, function() {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
+
+// async function initMap() {
+//   // The location of Uluru
+//   const position = { lat: -25.344, lng: 131.031 };
+//   // Request needed libraries.
+//   //@ts-ignore
+//   const { Map } = await google.maps.importLibrary("maps");
+//   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+//   // The map, centered at Uluru
+//   map = new Map(document.getElementById("map"), {
+//     zoom: 4,
+//     center: position,
+//     mapId: "DEMO_MAP_ID",
+//   });
+
+//   // The marker, positioned at Uluru
+//   const marker = new AdvancedMarkerElement({
+//     map: map,
+//     position: position,
+//     title: "Uluru",
+//   });
+// }
+
+// initMap();
