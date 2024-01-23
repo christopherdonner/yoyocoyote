0c42f896efc92374522375179620c103eacf1f5a
Christopher Donner <cdonner@opentext.com>
2024-01-23 09:20:36 -0500

    test commit branch2

--- db/seed.sql
+++ db/seed.sql
@@ -2,3 +2,4 @@ use coyote_db;
 insert into coyotes (coyoteName, longitude, latitude, dtime, active) values ("coyote1", '-80.5522517', '43.4807545', 1691001210727, true);
 insert into coyotes (coyoteName, longitude, latitude, dtime, active) values ("coyote2", '-80.546801', '43.480879', 1691001211627, true);
 insert into coyotes (coyoteName, longitude, latitude, dtime, active) values ("coyote3", '-80.555084', '43.476673', 1691877593300, true);
+insert into coyotes (coyoteName, longitude, latitude, dtime, active) values ("coyote4", '-80.555084', '43.476673', 1691877593300, true);
