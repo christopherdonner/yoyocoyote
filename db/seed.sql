809622b65737eb66d3ab75c1c4a9aca7197b1692
Christopher Donner <cdonner@opentext.com>
2025-02-11 13:20:37 -0500

    updated seed file

--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,4 @@
 use coyote_db;
-insert into coyotes (coyoteName, longitude, latitude, dtime, active) values ("coyote1", '-80.5522517', '43.4807545', 1691001210727, true);
-insert into coyotes (coyoteName, longitude, latitude, dtime, active) values ("coyote2", '-80.546801', '43.480879', 1691001211627, true);
-insert into coyotes (coyoteName, longitude, latitude, dtime, active) values ("coyote3", '-80.555084', '43.476673', 1691877593300, true);
+insert into coyotes (coyoteName, longitude, latitude, dtime, active, userid) values ("coyote1", '-80.5522517', '43.4807545', 1691001210727, true, 1);
+insert into coyotes (coyoteName, longitude, latitude, dtime, active) userid ("coyote2", '-80.546801', '43.480879', 1691001211627, true, 2);
+insert into coyotes (coyoteName, longitude, latitude, dtime, active) userid ("coyote3", '-80.555084', '43.476673', 1691877593300, true, 1);
